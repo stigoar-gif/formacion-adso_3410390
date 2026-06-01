2ac9d633c5d27e53317d3e995dd817d248e05f1c
Stiven <stigoar@gmail.com>
2026-06-01 11:36:44 -0500

    Modificacion

--- Trim1/Programacion/dart_adso/ejeCondicionalesComplementarios/condidobles/bin/eje2.dart
+++ Trim1/Programacion/dart_adso/ejeCondicionalesComplementarios/condidobles/bin/eje2.dart
@@ -1,7 +1,8 @@
 
 //Un propietario necesita calcular el impuesto anual de su vehículo. Las normativas fiscales establecen que los vehículos cuyo avalúo comercial supere los $150.000.000 (considerados de gama alta o lujo) deben pagar una tarifa del 2.5% sobre dicho valor. Por el contrario, los vehículos con un avalúo igual o menor a este límite pagan una tarifa del 1.5%. Solicite la marca/modelo del vehículo y su avalúo comercial actual. Determine cuál es el porcentaje aplicado y calcule el valor total del impuesto a pagar este año.
 
-import 'dart:async';
+// ignore_for_file: unused_local_variable
+
 import 'dart:io';
 
 void main(List<String> args) {
