8e1a638448d6c0ceabd0ecb4f276f5f5b010a527
Stiven <stigoar@gmail.com>
2026-06-01 08:06:53 -0500

    Ejemplo 1 complementarios dart

---
+++ Trim1/Programacion/dart_adso/ejeCondicionalesComplementarios/condsimple/bin/eje01.dart
@@ -0,0 +1,32 @@
+void main(List<String> arguments){
+//En el centro de formación CPIC, se requiere calcular el auxilio de transporte para los aprendices del programa ADSO. El auxilio base se calcula internamente multiplicando los días de asistencia en el mes por una tarifa fija de $5.000 diarios. Solicite al usuario el nombre del aprendiz, su municipio de residencia y los días que asistió al centro. Si el aprendiz vive fuera de "Manizales" (por ejemplo, en Villamaría o Neira), se le otorga un subsidio extra del 15% sobre el auxilio base para cubrir la distancia. Imprimir el nombre del aprendiz, el auxilio base calculado y el total a recibir.
+
+print("--- Gestión de auxilios de Transporte CPIC ---");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
